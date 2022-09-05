a921bf7bf1b4d656d54cc6f538af75c54b730a88
GopiQuarec <gopi.quarec@gmail.com>
2022-09-05 17:30:51 +0530

    Refact : refact code

--- server/app.js
+++ server/app.js
@@ -3,23 +3,16 @@ const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const express = require("express");
 const cookieParser = require("cookie-parser");
-
 const app = express();
 app.use(cookieParser());
-
-dotenv.config({ path: "./config.env" });
-
+dotenv.config({ path: ".env" });
 require("./db/conn");
 app.use(cors());
 app.options("*", cors());
-
 app.use(express.json());
-
 // we link the router files to make our route easy
 app.use(require("./router/route"));
-
-const PORT = process.env.PORT;
-
-app.listen(PORT, () => {
-  console.log(`server is runnig at port no ${PORT}`);
+// const PORT = process.env.PORT;
+app.listen(5000, () => {
+  console.log("server is runnig at port no");
 });
