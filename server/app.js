9514e427d12a3a7b8b365b1589658929f0daa9f9
GopiQuarec <gopi.quarec@gmail.com>
2022-09-05 15:47:23 +0530

    Secret17 server

---
+++ server/app.js
@@ -0,0 +1,25 @@
+const cors = require("cors");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const express = require("express");
+const cookieParser = require("cookie-parser");
+
+const app = express();
+app.use(cookieParser());
+
+dotenv.config({ path: "./config.env" });
+
+require("./db/conn");
+app.use(cors());
+app.options("*", cors());
+
+app.use(express.json());
+
+// we link the router files to make our route easy
+app.use(require("./router/route"));
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log(`server is runnig at port no ${PORT}`);
+});
---
+++ server/model/blogSchema.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: false,
+  },
+  article: {
+    type: String,
+    required: false,
+  },
+  authorname: {
+    type: String,
+    required: false,
+  },
+  articleImage: {
+    type: String,
+    required: false,
+  },
+  category: {
+    type: String,
+    required: false,
+  },
+});
+
+// collection creation
+const BlogPost = mongoose.model("BLOG", userSchema);
+module.exports = BlogPost;
