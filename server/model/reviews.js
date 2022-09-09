8508e3e19c6423edbd2968109e41766b7806b7b9
GopiQuarec <gopi.quarec@gmail.com>
2022-09-09 11:22:25 +0530

    changes

--- server/model/reviews.js
+++ server/model/reviews.js
@@ -2,20 +2,19 @@ const mongoose = require("mongoose");
 // const Schema = mongoose.Schema;
 
 const userSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-    },
-    email: {
-        type: String,
-        required: true,
-    },
-    message: {
-        type: String,
-        required: true,
-    },
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  message: {
+    type: String,
+    required: true,
+  },
 });
 
 const Review = mongoose.model("REVIEWS", userSchema);
-module.exports =Review;
-
+module.exports = Review;
